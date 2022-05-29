3e0f1421e1fd1ae8ee316a231779c237ebe2c640
dinaAlsaid <dinamalsaid@gmail.com>
2022-05-29 17:01:28 +0300

    todo item schema

--- src/services/journal/models/todoSchema.js
+++ src/services/journal/models/todoSchema.js
@@ -3,9 +3,13 @@ const mongoose = require("mongoose");
 
 const todo = mongoose.Schema({
   text: { type: String, required: true },
-  status: { type: String, required: true, default: "todo" },
-  importance: { type: Number, required: true },
-  assignee: { type: String, required: true },
+  status: { type: String, default: "todo", enum: ["todo", "doing", "done"] },
+  note: { type: String, required: false },
+  importance: { type: Boolean, required: true },
+  difficulty: { type: Number, required: false },
+  due: { type: Date, required: false },
+  //key:{type:string,enum},//enums are defined from the user
+  //   user: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },//not required for now untill authentication done
 });
-const journalDB = mongoose.connection.useDb('journal');
-module.exports = journalDB.model("todo", todo);
+const journalDB = mongoose.connection.useDb("journal");
+module.exports = journalDB.model("todoItem", todo);
