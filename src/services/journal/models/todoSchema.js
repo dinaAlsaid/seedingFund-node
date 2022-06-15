603c4ee082de8b77dbdca79e8a04171283894d4b
dinaAlsaid <dinamalsaid@gmail.com>
2022-06-15 11:56:08 +0300

    ?

--- src/services/journal/models/todoSchema.js
+++ src/services/journal/models/todoSchema.js
@@ -4,9 +4,9 @@ const mongoose = require("mongoose");
 const todo = mongoose.Schema({
   text: { type: String, required: true, unique: true },
   status: { type: String, default: "todo", enum: ["todo", "doing", "done"] },
-  note: { type: String, required: false },
   importance: { type: Boolean, required: true, default: false },
   difficulty: { type: Number, required: false },
+  note: { type: String, required: false },
   due: { type: Date, required: false },
   //key:{type:string,enum},//enums are defined from the user
   //   user: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },//not required for now untill authentication done
